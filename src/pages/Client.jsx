722d5e75a5e5bb539b8b7372ae73cf5c876fd255
touil-mohamed <touil.mohamed1@hotmail.com>
2024-07-04 11:46:38 +0200

    feat: :art: add interface clients

---
+++ src/pages/Client.jsx
@@ -0,0 +1,19 @@
+import Sidebar from "../components/sideBar/Sidebar.jsx";
+import User from "../components/user/User.jsx";
+import "./Client.css";
+
+const Client = () => {
+  return (
+    <div className="dashboard">
+      <Sidebar />
+      <div className="main-container">
+        <header className="header">
+          <User />
+        </header>
+        <main className="main-content"></main>
+      </div>
+    </div>
+  );
+};
+
+export default Client;
